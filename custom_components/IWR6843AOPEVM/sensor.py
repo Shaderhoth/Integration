35ee6270a20513f517dfadce4bc99ac8da5f6e76
david <davidkuc6@gmail.com>
2024-12-12 23:27:05 +0000

    handled errors

--- custom_components/IWR6843AOPEVM/sensor.py
+++ custom_components/IWR6843AOPEVM/sensor.py
@@ -5,24 +5,18 @@ from datetime import timedelta
 from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the radar sensor platform."""
     core = hass.data[DOMAIN][config_entry.entry_id]
-
-    # Set up a data update coordinator for periodic updates
     coordinator = RadarDataUpdateCoordinator(hass, core)
     await coordinator.async_config_entry_first_refresh()
-
     async_add_entities([RadarSensor(coordinator, config_entry.data[CONF_NAME])])
 
 class RadarDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching radar data periodically."""
-
     def __init__(self, hass, core):
         super().__init__(
             hass,
             logger=hass.helpers.logger,
             name="Radar Sensor Data",
-            update_interval=timedelta(seconds=0.2),
+            update_interval=timedelta(milliseconds=200),
         )
         self.core = core
 
@@ -30,13 +24,17 @@ class RadarDataUpdateCoordinator(DataUpdateCoordinator):
         return self.core.parseData()
 
 class RadarSensor(CoordinatorEntity, SensorEntity):
-    """Representation of a radar sensor."""
-
     def __init__(self, coordinator, name):
         super().__init__(coordinator)
         self._attr_name = name
         self._attr_native_unit_of_measurement = "people"
+        self._last_valid_value = None
 
     @property
     def native_value(self):
-        return self.coordinator.data
+        return self._last_valid_value
+
+    async def async_update(self):
+        new_value = await self.coordinator._async_update_data()
+        if new_value != -1:
+            self._last_valid_value = new_value
